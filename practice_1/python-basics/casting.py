7d64ed297c9a3439786927405a0f5005700a6877
tilektes-ismail <tilektesismail@gmail.com>
2026-02-04 16:16:28 +0500

    add casting

---
+++ practice_1/python-basics/casting.py
@@ -0,0 +1,18 @@
+x = "10"
+y = int(x)
+print(y + 5)   # 15
+
+a = "3.5"
+b = float(a)
+print(b * 2)   # 7.0
+
+age = 18
+age_str = str(age)
+print("Age: " + age_str)
+
+x = 9.8
+y = int(x)
+print(y)   # 9
+
+print(int(True))   # 1
+print(int(False))  # 0
